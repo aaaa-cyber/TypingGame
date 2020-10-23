8acfa1ded0d928c18f3859993d7c6b549c2079a5
林 <hayashi@Macbook.local>
2020-10-23 22:40:54 +0900

    Optimized quiz generation

--- tools/prepare.java
+++ tools/prepare.java
@@ -3,13 +3,21 @@ package tools;
 import java.io.FileReader;
 import java.io.BufferedReader;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
 
 public class prepare {
-    public static String[] quiz(int num) {
-        Random rand = new Random();
-        String[] ret = new String[num];
+    public static ArrayList<String> textToAryList(String filePass) {
+        ArrayList<String> list = new ArrayList<>();
+        try {
+            BufferedReader lr = new BufferedReader(new FileReader(filePass));
+            while (lr.readLine() != null) list.add(lr.readLine());
+            lr.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return list;
+    }
+
+    public static ArrayList<String> quizList() {
         String file;
         int switchQuiz = 0;
         System.out.print("0...英単語/1...数字/2...かな : ");
@@ -24,28 +32,6 @@ public class prepare {
             case 2 : file = "kana.txt"; break;
             default : file = "words.txt";
         }
-        try {
-            BufferedReader lr = new BufferedReader(new FileReader("quiz/" + file));
-            List<String> list = new ArrayList<String>();
-            while (lr.readLine() != null) list.add(lr.readLine());
-            lr.close();
-            int[] randomNum = new int[num];
-            //乱数の生成
-            for (int i = 0; i < num; i++) {
-                LOOP : while (true) {
-                    randomNum[i] = rand.nextInt(list.size()) + 1;
-                    //重複チェック
-                    if (i == 0) break;
-                    for (int j = 0; j < i; j++) {
-                        if (randomNum[j] == randomNum[i]) break;
-                        if (j == i - 1) break LOOP;
-                    }
-                }
-                ret[i] = list.get(randomNum[i]);
-            }
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-        return ret;
+        return textToAryList("quiz/" + file);
     }
 }
--- tools/run.java
+++ tools/run.java
@@ -1,16 +1,23 @@
 package tools;
 
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Random;
 
 public class run {
     public static Scanner stdIn = new Scanner(System.in);
     public static void quiz() {
+        Random rand = new Random();
         int howMany = 3;
-        String[] question = prepare.quiz(howMany);
+        ArrayList<String> list = prepare.quizList();
         for (int i = 0; i < howMany; i++) {
+            int randomNum = rand.nextInt(list.size());
             while (true) {
-                System.out.println(question[i]);
-                if (stdIn.next().equals(question[i])) break;
+                System.out.println(list.get(randomNum));
+                if (stdIn.next().equals(list.get(randomNum))) {
+                    list.remove(randomNum);
+                    break;
+                }
             }
         }
     }
